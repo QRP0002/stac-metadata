ae4309b2f19edfdd51c917832ecda266a78321aa
Quinn Pommerening <quinn.pommerening@pm.me>
2025-05-26 21:04:02 -0600

    feat: add ability to call sciencebase api to read in data

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 import pystac
 
 from datetime import datetime
+from dotenv import load_dotenv
 
 from entities.catalog import Catalog
 from entities.collection import Collection
@@ -10,8 +11,10 @@ from components.catalog import StacCatalog
 from components.collection import StacCollection
 from components.item import StacItem
 from cloud.upload_stac import UploadStac
+from http_requests.http_requests import HttpRequests
 
 with open('../data/config.json') as config:
+    load_dotenv()
     _config = json.load(config)
 
     _catalog = _config['catalog']
@@ -21,37 +24,36 @@ with open('../data/config.json') as config:
 
     for _coll in _config['collections']:
         _collection = _coll['collection']
-        print('COLLECTION: ', _collection)
         sc = StacCollection()
         tc = Collection(_collection['id'], _collection['description'],
             _collection['title'])
 
         items_cache = []
         _items = _coll['items']
-        with open(_items['data']) as f:
-            d = json.load(f)
-            for sb_item in d['items']:
-                item = StacItem(sb_item['id'], sb_item['title'], sb_item['summary']).item
-                items_cache.append(item)
 
-                # Compare item data to collection data to get the most accurate information
-                _start = sc.compare_start_date(tc.start_datetime, 
-                    item.properties['start_datetime'])
-                tc.start_datetime = _start
+        url = f'{os.environ['SB_BASE_URL']}/{_items['sb_query']}'
+        response = HttpRequests.get_request(url)
+        for sb_item in response['items']:
+            item = StacItem(sb_item['id'], sb_item['title'], sb_item['summary']).item
+            items_cache.append(item)
 
-                _end = sc.compare_end_date(tc.end_datetime, 
-                    item.properties['end_datetime'])
-                tc.end_datetime = _end
+            # Compare item data to collection data to get the most accurate information
+            _start = sc.compare_start_date(tc.start_datetime, 
+                item.properties['start_datetime'])
+            tc.start_datetime = _start
 
-                for tag in item.properties['tags']:
-                    tc.keys.add(tag)
+            _end = sc.compare_end_date(tc.end_datetime, 
+                item.properties['end_datetime'])
+            tc.end_datetime = _end
 
-                tc.bbox.min_x = sc.find_min(tc.bbox.min_x, item.bbox[0])
-                tc.bbox.min_y = sc.find_min(tc.bbox.min_y, item.bbox[1])
-                tc.bbox.max_x = sc.find_max(tc.bbox.max_x, item.bbox[2])
-                tc.bbox.max_y = sc.find_max(tc.bbox.max_y, item.bbox[3])
+            for tag in item.properties['tags']:
+                tc.keys.add(tag)
+
+            tc.bbox.min_x = sc.find_min(tc.bbox.min_x, item.bbox[0])
+            tc.bbox.min_y = sc.find_min(tc.bbox.min_y, item.bbox[1])
+            tc.bbox.max_x = sc.find_max(tc.bbox.max_x, item.bbox[2])
+            tc.bbox.max_y = sc.find_max(tc.bbox.max_y, item.bbox[3])
         
-            f.close()
 
         collection = sc.create_stac_collection(tc)
         collection = sc.add_items(items_cache, collection)
@@ -60,7 +62,7 @@ with open('../data/config.json') as config:
         catalog.normalize_hrefs(f'../output/{catalog.id}')
         catalog.save(catalog_type=pystac.CatalogType.SELF_CONTAINED)
 
-        UploadStac().upload_catalog(catalog.id, f'../output/{catalog.id}/catalog.json')
+        # UploadStac().upload_catalog(catalog.id, f'../output/{catalog.id}/catalog.json')
 
         collection_path = f'../output/{catalog.id}'
         collection_dir_path =f'{catalog.id}'
@@ -69,12 +71,13 @@ with open('../data/config.json') as config:
             coll_path = f'{collection_dir_path}/{coll.id}'
             local_coll_path = f'{collection_path}/{coll.id}'
 
-            UploadStac().upload_collection(coll_path, f'{local_coll_path}/collection.json')
+            # UploadStac().upload_collection(coll_path, f'{local_coll_path}/collection.json')
 
             # Add collection item meta data
             for item in coll.get_items():
                 item_bucket = f'{coll_path}/{item.id}'
                 local_item_path = f'{local_coll_path}/{item.id}/{item.id}.json'
         
-                UploadStac().upload_item(item_bucket, local_item_path)
+                # UploadStac().upload_item(item_bucket, local_item_path)
     config.close()
+print("We did it Reddit!")
